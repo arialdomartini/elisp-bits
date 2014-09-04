430d950cf13815967f8df9d084dbd27e8e9134cd
Arialdo Martini <arialdomartini@gmail.com>
2014-09-04 09:10:24 +0200

    Refactoring: passing point-min and point-max as an argument to a common function

--- buffers.el
+++ buffers.el
@@ -1,13 +1,16 @@
+(defun move-somewhere-pushing-mark (which-point)
+  (push-mark)
+  (goto-char (funcall which-point))
+)
+
 (defun simplified-beginning-of-buffer ()
   "Move the point to the beginning of buffer"
   (interactive)
-  (push-mark)
-  (goto-char (point-min))
+  (move-somewhere-pushing-mark 'point-min)
 )
 
 (defun simplified-end-of-buffer ()
   "Move the point to the last character of the buffer"
   (interactive)
-  (push-mark)
-  (goto-char (point-max))
+  (move-somewhere-pushing-mark 'point-max)
 )
