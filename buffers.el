c08bb04787c5532cd090ebb9f533831cd27444be
Arialdo Martini <arialdomartini@gmail.com>
2014-09-04 08:48:21 +0200

    A simplified version of (end-of-buffer)

--- buffers.el
+++ buffers.el
@@ -4,3 +4,10 @@
   (push-mark)
   (goto-char (point-min))
 )
+
+(defun simplified-end-of-buffer ()
+  "Move the point to the last character of the buffer"
+  (interactive)
+  (push-mark)
+  (goto-char (point-max))
+)
