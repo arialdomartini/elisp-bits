e0b69943d06ae01524696f47a341a05c7c3692a3
Arialdo Martini <arialdomartini@gmail.com>
2014-09-05 09:17:33 +0200

    A simplified version of (mark-whole-buffer)

--- buffers.el
+++ buffers.el
@@ -14,3 +14,12 @@
   (interactive)
   (move-somewhere-pushing-mark 'point-max)
 )
+
+(defun simplified-mark-whole-buffer ()
+  "Put the point at the beginning and the mark at the end of buffer."
+  (interactive)
+  (push-mark (point) nil t)
+  (push-mark (point-max) nil t)
+  (goto-char (point-min))
+)
+
