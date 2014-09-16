7c4db1705d499ead0ea444464d9baf88205716df
Arialdo Martini <arialdomartini@gmail.com>
2014-09-16 10:26:20 +0200

    car and cdr of list of lists

--- carcdr.el
+++ carcdr.el
@@ -7,3 +7,13 @@
 
 (first '(1 2 3 4 5))
 (rest  '(1 2 3 4 5))
+
+
+(car '((1 2 3) ("a" "b" "c") (1.1 2.2 3.3)))
+(cdr '((1 2 3) ("a" "b" "c") (1.1 2.2 3.3)))
+
+(car '(
+       (lion tiger cheetah)
+       (gazelle antelope zebra)
+       (whale dolphin seal)
+       ))
