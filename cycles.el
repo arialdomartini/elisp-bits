674433d0b97341d29e8096e029a0287ee0451fbf
Arialdo Martini <arialdomartini@gmail.com>
2014-12-03 19:37:10 +0100

    Writing something in another buffer

--- cycles.el
+++ cycles.el
@@ -8,3 +8,31 @@
 
 
 (print-elements-of-a-list '(one "two" three)) 
+
+
+(defun get-last-element (list)
+  (if (eq (cdr list) nil)
+      (car list)
+    (get-last-element (cdr list))
+    )
+  )
+
+(get-last-element '(a b c d e f ))
+
+(defun print-something-in-a-new-buffer (buffer something)
+  (let ((oldbuffer (current-buffer)))
+    (save-excursion
+      (get-buffer-create buffer)
+      (set-buffer buffer)
+      (insert "ciao")
+      (set-buffer oldbuffer)
+      (message "Created a buffer names %s" buffer)
+      )
+    )
+  )
+
+
+(print-something-in-a-new-buffer "another-buffer" "write me")
+
+
+
