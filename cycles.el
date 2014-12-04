d2da73892b9cba3ea24a599555303ad22c2ff6cf
Arialdo Martini <arialdomartini@gmail.com>
2014-12-04 08:39:34 +0100

    Experimenting with lists

--- cycles.el
+++ cycles.el
@@ -7,7 +7,14 @@
   )
 
 
-(print-elements-of-a-list '(one "two" three)) 
+(defun print-elements-of-a-list (list)
+  "Print each elements of a list on a line of its own"
+  (while list
+    (print (car list))
+    (setq list (cdr list))
+    )
+  )
+
 
 
 (defun get-last-element (list)
@@ -18,6 +25,81 @@
   )
 
 (get-last-element '(a b c d e f ))
+
+
+(defun is-empty(list)
+  (eq list nil)
+  )
+
+(is-empty ())
+(is-empty '(a b c))
+
+
+ 
+(is-empty '())
+
+
+(defun second-element (list)
+  (car (cdr list))
+  )
+
+(second-element ())
+(second-element '(a))
+(second-element '(a b))
+(second-element '(a b c))
+(second-element '(a b c d ))
+
+
+(defun first-element (list)
+  (car list)
+  )
+
+(first-element '(a))
+(first-element '(a b))
+(first-element '(a b c))
+(first-element '(a b c d ))
+
+
+
+(defun invert-two-elements (list)
+  (cons (cdr list) (list (car list)))
+  )
+
+(invert-two-elements '(a b))
+(invert-two-elements '(a b c))
+
+
+(defun has-only-one-element (list)
+  (and
+   (eq (cdr list) nil)
+   (not (eq (car list) nil))
+   
+   )
+  )
+
+(defun has-no-more-than-one-element (list)
+   (eq (cdr list) nil)
+  )
+
+
+(defun invert(list)
+  (if (has-no-more-than-one-element list)
+      (car list)
+    (list (invert (cdr list)) (car list))
+    )
+  )
+
+(invert '(1 2 3 4 5))
+
+(let (( lista '(1 2 3 4 5) ))
+  (setq first (car lista))
+  (setq rest (cdr lista))
+  (list lista first)
+     )
+
+
+(invert '(a b))
+
 
 (defun print-something-in-a-new-buffer (buffer something)
   (let ((oldbuffer (current-buffer)))
