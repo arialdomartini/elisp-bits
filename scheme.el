fa81e0a1d9961ecd25851b649f0bb8e14b9da69c
Arialdo Martini <arialdomartini@gmail.com>
2016-05-24 20:30:12 +0200

    firsts extracts the first elements of a list; recur is like map

--- scheme.el
+++ scheme.el
@@ -25,3 +25,47 @@
           (8 9 10)
           )
         )
+
+(firsts '(
+          ((1 2) 3 4)
+          ((10 20) 30 40)
+          (9 10)
+          ))
+
+
+(defun double (lat)
+  (cond
+   ((eq lat ()) ())
+   (t (cons
+       (* 2 (car lat))
+       (double (cdr lat))))))
+
+(double '(1 2 3 4))
+
+
+
+
+(defun doublen (n)
+  (* 2 n))
+
+(doublen 12)
+
+
+
+(defun call-d ()
+  (funcall 'doublen 10))
+
+(call-d )
+
+; recur is like map: it's a higher function that applies a function to a list
+(defun recur (f lat)
+  (cond
+   ((eq lat ()) ())
+   (t (cons
+       (funcall f (car lat))
+       (recur f (cdr lat))))
+   )
+  )
+
+(recur (lambda (x) (doublen x)) '(1 2 3 4))
+(recur 'doublen '(1 2 3 4))
